db2b7d77e3e9b6444cc9dd3285e133f3ab73b222
lionel-SF <oye.lionel@sf-express.com>
2016-01-25 20:43:55 +0800

    Test for git lionel-SF

--- Demo1_HelloWorld/AppDelegate.h
+++ Demo1_HelloWorld/AppDelegate.h
@@ -10,7 +10,10 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-@property (nonatomic, strong) UIViewController *viewController;
+@property(strong, nonatomic) UIWindow *window;
+@property(nonatomic, strong) UIViewController *viewController;
+@property(nonatomic, copy) NSString *name;
+@property(nonatomic, retain) NSObject *editable;
+@property(assign, getter=isEditable) BOOL canEditable;
 
 @end
